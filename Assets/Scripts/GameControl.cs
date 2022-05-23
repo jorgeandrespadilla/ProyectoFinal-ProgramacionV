275369a3685bc1cd461ef74738ed571f6930f573
Victor Ponce <poncev@gmail.com>
2022-05-22 19:08:13 -0500

    Add SpaceShip Instantiate

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class GameControl : MonoBehaviour
 {
-    
+    public GameObject[ ]  characters;
+    public string MenuScene = "SelectionMenu";
+    private string selectedCharacterDataName = "SelectedCharacter";
+    int selectedCharacter;
+    public GameObject playerObject;
     // Start is called before the first frame update
     void Start()
     {
+        selectedCharacter = PlayerPrefs.GetInt(selectedCharacterDataName,0);
+        
+        var spaceship = Instantiate(characters[selectedCharacter]);
+        spaceship.transform.parent = playerObject.transform;
+
         
     }
+    
 
     // Update is called once per frame
     void Update()
