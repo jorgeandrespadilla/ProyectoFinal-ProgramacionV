4cb9db0455f72578e863f1ba7ee2b600c8db13e3
Daniel Bustos <daniel.bustos.velez@udla.edu.ec>
2022-07-03 00:23:37 -0500

    Enemy generator added

--- Assets/Scripts/Characters/CanvasMovement.cs
+++ Assets/Scripts/Characters/CanvasMovement.cs
@@ -3,16 +3,17 @@ using UnityEngine;
 public class CanvasMovement : MonoBehaviour
 {
     [SerializeField]
-    GameObject _object;
+    GameObject mainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = GameObject.FindWithTag("MainCamera");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.rotation = Quaternion.LookRotation(transform.position - _object.transform.position);
+        transform.rotation = Quaternion.LookRotation(transform.position - mainCamera.transform.position);
     }
 }
